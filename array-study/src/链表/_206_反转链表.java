bb5b25b84bbcdb2e2031cfff21d93cb751ded286
wanghe <wanghe-sudo@outlook.com>
2023-07-27 11:20:13 +0800

    Add:提交力扣解题

---
+++ array-study/src/链表/_206_反转链表.java
@@ -0,0 +1,44 @@
+package 链表;
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class _206_反转链表 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode() {
+        }
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode reverseList(ListNode head) {
+        // 传入head，返回新head
+        // 递归和非递归
+
+        // 0或者1个节点
+        if (head == null || head.next == null) return head;
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+
+    }
+
+}
---
+++ array-study/src/链表/_237_删除链表中的节点.java
@@ -0,0 +1,22 @@
+package 链表;
+
+public class _237_删除链表中的节点 {
+    public class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public void deleteNode(ListNode node) {
+        node.val = node.next.val;
+        node.next = node.next.next;
+    }
+
+    public static void main(String[] args) {
+
+    }
+
+}
